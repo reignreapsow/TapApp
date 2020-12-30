bf8c507be530dd38f40a748488812f738f9e5fd8
18231550+reignreapsow@users.noreply.github.com <18231550+reignreapsow@users.noreply.github.com>
2020-12-30 02:14:38 -0500

    stabilized git

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 
-
-import {RecordVoiceOver, Face, Fingerprint } from "@material-ui/icons";
+import Face from "./../imgs/icons/face-24px.svg";
+import Fingerprint from "./../imgs/icons/fingerprint-24px.svg";
+import Mic from "./../imgs/icons/mic-24px.svg";
 
 import Home from "./Dashboard"; //RENAMED DASHBOARD TO HOME
 import logo from "./../imgs/sexy_silhouette_2.png";
@@ -23,31 +24,44 @@ function login() {
 function LoginScreen() {
   return (
     <div className="loginScreen">
-      <img className="loginScreen__logo"  src={logo} alt="tap-logo" />
+      <img className="loginScreen__logo" src={logo} alt="tap-logo" />
       <h1>Login</h1>
       <form>
-        <input type="password" maxLength="18"  />
-        <button onClick={login()} type="submit" value="submit" className="loginScreen__btn" >Login</button>
+        <input type="password" maxLength="18" />
+        <button
+          onClick={login()}
+          type="submit"
+          value="submit"
+          className="loginScreen__btn"
+        >
+          Login
+        </button>
       </form>
       <div className="alt__login">
-        <img
-          src={Fingerprint}
-          fontSize="large"
-          className="fingerPrintScanner"
-          alt="fingerprint"
-        />
-        <img src={Face}
-        fontSize="large" 
-        className="faceScan" 
-        alt="face" 
-
-        />
-        <img 
-            src={RecordVoiceOver}
-            fontSize="large" 
-            className="voiceCheck" 
-            alt="voice" 
-            />
+        <Link to>
+          <img
+            src={Fingerprint}
+            fontSize="large"
+            className="quickLogin_btn"
+            alt="fingerprint"
+          />
+        </Link>
+        <Link to>
+          <img
+            src={Face}
+            fontSize="large"
+            className="quickLogin_btn"
+            alt="face"
+          />
+        </Link>
+        <Link to>
+          <img
+            src={Mic}
+            fontSize="large"
+            className="quickLogin_btn"
+            alt="voice"
+          />
+        </Link>
       </div>
     </div>
   );
