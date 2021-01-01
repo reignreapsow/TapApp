7a3a78c1af4b8c26f734461c22715965e9abd363
18231550+reignreapsow@users.noreply.github.com <18231550+reignreapsow@users.noreply.github.com>
2020-12-31 20:57:27 -0500

    Login Screen first draft

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-import { Dashboard } from '@material-ui/icons'
+// import { Dashboard } from '@material-ui/icons'
 
 import './../scss/components/Nav.scss'
 
@@ -9,10 +9,10 @@ function Nav() {
     return (
         <nav>
             <ul>
-                <li><Link to="/home">{Dashboard}</Link></li>
-                <li><Link to="/browse">Browse</Link></li>
-                <li><Link to="/match">Match</Link></li>
-                <li><Link to="/settings">Settings</Link></li>
+                <li><Link to="/home" className="NavLink">Dashboard</Link></li>
+                <li><Link to="/browse" className="NavLink">Browse</Link></li>
+                <li><Link to="/match" className="NavLink">Match</Link></li>
+                <li><Link to="/settings" className="NavLink">Settings</Link></li>
             </ul>
         </nav>
     )
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,11 +5,9 @@ import Nav from "./../components/Nav";
 
 import "./../scss/pages/Dashboard.scss";
 
-
-
 function Dashboard() {
   return (
-    <div className="dashboard"> 
+    <div className="dashboard">
       <h2>Dashboard Page</h2>
       {/* <Header /> */}
       <Nav />
